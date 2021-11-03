83861791ea7215cbf6fd7b05b7b7a04310ff473e
kim hyoju <90176069+POOHYA@users.noreply.github.com>
2021-11-03 11:57:34 +0900

    contents domain 업데이트

--- OTTe_search_new/src/main/java/org/zerock/domain/ContentsVO.java
+++ OTTe_search_new/src/main/java/org/zerock/domain/ContentsVO.java
@@ -4,64 +4,42 @@ import java.util.Date;
 
 import lombok.Data;
 
-@Data
-public class ContentsVO {
+public @Data class ContentsVO {
 
-	private Integer contents_id;
-	private Integer nation_id;
-	private Integer ott_id;
-	private Integer grade_id;
-	private Integer class_id;
+	private Integer contentsid;
+	private Integer nationid;
+	private Integer ottid;
+	private Integer gradeid;
+	private Integer classid;
 	private String title;
 	private String summary;
+	private Date years;
 	private Integer season;
 	private Integer episode;
 	private Integer score;
 	private Integer duration;
-	private Date madeDate;
 	
 	/*
-	public Integer getNum() {
-		return num;
-	}
-	public void setNum(Integer num) {
-		this.num = num;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Integer getReadCount() {
-		return readCount;
-	}
-	public void setReadCount(Integer readCount) {
-		this.readCount = readCount;
-	}
-	public Date getWriteDate() {
-		return writeDate;
-	}
-	public void setWriteDate(Date writeDate) {
-		this.writeDate = writeDate;
-	}
-	@Override
-	public String toString() {
-		return "BoardVO [num=" + num + ", name=" + name + ", title=" + title + ", content=" + content + ", readCount="
-				+ readCount + ", writeDate=" + writeDate + "]";
-	}
-*/
+	 * public Integer getContentsid() { return contentsid; } public void
+	 * setContentsid(Integer contentsid) { this.contentsid = contentsid; } public
+	 * Integer getNationid() { return nationid; } public void setNationid(Integer
+	 * nationid) { this.nationid = nationid; } public Integer getOttid() { return
+	 * ottid; } public void setOttid(Integer ottid) { this.ottid = ottid; } public
+	 * Integer getGradeid() { return gradeid; } public void setGradeid(Integer
+	 * gradeid) { this.gradeid = gradeid; } public Integer getClassid() { return
+	 * classid; } public void setClassid(Integer classid) { this.classid = classid;
+	 * } public String getTitle() { return title; } public void setTitle(String
+	 * title) { this.title = title; } public String getSummary() { return summary; }
+	 * public void setSummary(String summary) { this.summary = summary; } public
+	 * Integer getSeason() { return season; } public void setSeason(Integer season)
+	 * { this.season = season; } public Integer getEpisode() { return episode; }
+	 * public void setEpisode(Integer episode) { this.episode = episode; } public
+	 * Integer getScore() { return score; } public void setScore(Integer score) {
+	 * this.score = score; } public Integer getDuration() { return duration; }
+	 * public void setDuration(Integer duration) { this.duration = duration; }
+	 * public Date getYears() { return years; } public void setYears(Date years) {
+	 * this.years = years; }
+	 */
+
 	
 }
